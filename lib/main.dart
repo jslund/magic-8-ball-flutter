351e8b02fde620390020c6968fcf926d7fe0ffd4
James Lund <jlund@adgistics.com>
2020-02-19 20:50:47 +0000

    First Edit

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,39 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(
       MaterialApp(
-        home: null,
+        home: Scaffold(
+          appBar: AppBar(
+            title: Text("Ask Me Anything"),
+          ),
+          body: BallPage(),
+        ),
       ),
     );
+
+
+class BallPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.blue,
+      child: Column(
+        children: <Widget>[
+          Ball(),
+          Text("")
+        ],
+      ),
+    );
+  }
+}
+
+class Ball extends StatefulWidget {
+  @override
+  _BallState createState() => _BallState();
+}
+
+class _BallState extends State<Ball> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
